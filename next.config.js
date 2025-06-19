dd35a6e427dd2ae0156a5c987e903bdf8f61fa72
Muhammad Sameer <sameermemon834@gmail.com>
2025-06-19 22:37:13 +0500

    changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-/** @type {import('next').NextConfig} */
 const path = require('path');
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ disables ESLint during Vercel builds
+  },
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     return config;
